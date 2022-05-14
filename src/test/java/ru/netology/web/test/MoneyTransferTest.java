38ce74e8216543065f68dd409220849c40b3aef5
nickola102 <nickola@list.ru>
2022-05-14 14:44:03 +0300

    new test3

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -30,14 +30,14 @@ class MoneyTransferTest {
     @Test
     void shouldTransferMinAmount1() {
         var dashboardPage = new DashboardPage();
-        val initialBalanceCard1 = dashboardPage.getSecondCardBalance();
-        val initialBalanceCard2 = dashboardPage.getFirstCardBalance();
+        val initialBalanceCard1 = dashboardPage.getFirstCardBalance();
+        val initialBalanceCard2 = dashboardPage.getSecondCardBalance();
         dashboardPage.firstCard();
         int money = 1;
         var transferPage = new TransferPage();
         transferPage.transferMoney(money, card2);
         int actualCard1 = dashboardPage.getFirstCardBalance();
-        int actualCard2  = dashboardPage.getSecondCardBalance();
+        int actualCard2 = dashboardPage.getSecondCardBalance();
 //        int actualCard1 = dashboardPage.getCardBalance(0);
 //        int actualCard2 = dashboardPage.getCardBalance(1);
         int expectedCard1 = initialBalanceCard1 - money;
