3791cfc6457f2b44bbf16c9bb521020586f180bf
nickola102 <nickola@list.ru>
2022-05-15 12:32:42 +0300

    update test

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -96,23 +96,6 @@ class MoneyTransferTest {
     }
 
     @Test
-    void shouldTransferNearMaxLimit1() {
-        var dashboardPage = new DashboardPage();
-        int initialBalanceCard1 = dashboardPage.getCardBalance(0);
-        int initialBalanceCard2 = dashboardPage.getCardBalance(1);
-        dashboardPage.firstCard();
-        int money = initialBalanceCard2 - 1;
-        var transferPage = new TransferPage();
-        transferPage.transferMoney(money, card2);
-        int actualCard1 = dashboardPage.getCardBalance(0);
-        int actualCard2 = dashboardPage.getCardBalance(1);
-        int expectedCard1 = initialBalanceCard1 + money;
-        int expectedCard2 = initialBalanceCard2 - money;
-        assertEquals(expectedCard1, actualCard1);
-        assertEquals(expectedCard2, actualCard2);
-    }
-
-    @Test
     void shouldTransferMax() {
         var dashboardPage = new DashboardPage();
         int initialBalanceCard1 = dashboardPage.getCardBalance(0);
